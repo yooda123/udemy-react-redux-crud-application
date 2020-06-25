a9126eb4528ed643947f154516c918d590737e3a
yooda123 <yosukeee.o@gmail.com>
2020-06-25 13:09:10 +0900

    more jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 // import logo from './logo.svg';
 
 function App() {
+  const aaa = 'Ooooops!';
+  const dom = <h1 className="foo" onClick={()=>{console.log("clicked.")}}>Hello, world {aaa}.</h1>;
   return (
-    <h1>
-Hello, world.
-    </h1>
+    dom
   );
 }
 
